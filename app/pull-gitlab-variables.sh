76f22e0f8c494133b845b8387cf5b5ae9865a961
Maciej PIetrzykowski <maciej.pietrzykowski@getindata.com>
2023-06-02 12:12:33 +0200

    feat: Comment Not allowed envs

--- app/pull-gitlab-variables.sh
+++ app/pull-gitlab-variables.sh
@@ -49,7 +49,8 @@ do
     content=$(echo $v | jq -r '.value')
     MULTIENV_RESULT+="${name}=${content}",
   else
-    >&2 echo "Not allowed environment variable: $name. Skipped..."
+# TODO better logging
+#    >&2 echo "Not allowed environment variable: $name. Skipped..."
   fi
 done
 
